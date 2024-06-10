b000ff1ae3d39312a0aa227b1ef9cf58bbb8cdaa
e-kondra <e-kondra@mail.ru>
2024-06-10 17:40:19 +0300

    first commit. Creating proj

---
+++ src/main/java/andersen/AndersenTasks/AndersenTasksApplication.java
@@ -0,0 +1,13 @@
+package andersen.AndersenTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndersenTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndersenTasksApplication.class, args);
+	}
+
+}
