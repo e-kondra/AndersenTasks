0e656faf3468dede2a23c63a9bed7f33b0319414
e-kondra <e-kondra@mail.ru>
2024-07-15 09:52:12 +0300

    correction

--- src/main/java/andersen/AndersenTasks/AndersenTasksApplication.java
+++ src/main/java/andersen/AndersenTasks/AndersenTasksApplication.java
@@ -54,65 +54,6 @@ public class AndersenTasksApplication {
 		userService.deleteUserById(3);
 
 
-
-
-		TicketService ticketService = new TicketService();
-		//ticketService.getTicketStorage().forEach(System.out::println);
-
-		BusTicketService busTicketService = new BusTicketService();
-		busTicketService.busTicketStorage.add(busTicketService.createBusTicketByDateAndType(LocalDate.now(), BusTicketType.DAY));
-		busTicketService.saveBusTicketInStorage(
-				busTicketService.createBusTicketByDateAndType(LocalDate.of(2024, Month.JANUARY,12), BusTicketType.YEAR));
-		busTicketService.busTicketStorage
-				.add(busTicketService.createBusTicketByDateAndType(LocalDate.of(2024,6,18), BusTicketType.MONTH));
-		busTicketService.busTicketStorage
-				.add(new BusTicket(1L, LocalDate.of(2023,12,10), BusTicketType.YEAR, 200));
-		busTicketService.saveBusTicketInStorage(
-				new BusTicket(2L, LocalDate.of(2023,10,1), BusTicketType.YEAR, 6000));
-		busTicketService.saveBusTicketInStorage(
-				new BusTicket( 3L, LocalDate.of(2024,6,17), BusTicketType.WEEK, 1200));
-
-		busTicketService.busTicketStorage.forEach(System.out::println);
-
-		Optional<BusTicket> busTicket = busTicketService.getBusTicketById(1L);
-		System.out.println("busTicket get by id = 1 " + busTicket.get());
-
-		busTicketService.removeBusTicketById(2L);
-		System.out.println("---- remove ticket with id = 2");
-		busTicketService.busTicketStorage.forEach(System.out::println);
-
-		ArrayList<BusTicket> tickets = busTicketService.getTicketsByTypeAndPrice(BusTicketType.YEAR, 0, 1000);
-		System.out.println("---- getTicketsByTypeAndPrice. TYPE=YEAR, price = from 0 to 1000");
-		tickets.forEach(System.out::println);
-
-
-//		TicketService ticketService = new TicketService();
-		//ticketService.getTicketStorage().forEach(System.out::println);
-
-//		Ticket ticket = new Ticket();
-//		ticket.setProtoId(12L);
-//
-//		ticket.print();
-//
-//		ticket.share("+37068361010");
-//		ticket.share("+37068361010","e-kondra@gmail.com");
-//
-//		Admin admin = new Admin();
-//		admin.printRole();
-//		admin.checkTicket(ticket);
-//
-//		Client client = new Client(ticket);
-//		client.printRole();
-//		client.getTicket();
-//
-//		Ticket ticket1 = new Ticket();
-//		ticket1.setProtoId(12L);
-//		System.out.println(ticket.equals(ticket1));
-//		System.out.println(ticket.hashCode());
-//		System.out.println(ticket1.hashCode());
-//
-//		NullableWarningProcessor.fieldCheck(ticket1);
-
 	}
 
 }
